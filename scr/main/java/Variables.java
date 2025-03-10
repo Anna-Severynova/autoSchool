9ba925c9913168d7039816d04c5f62fdc89aae6e
anna.severynova <anna.severynova@nixs.com>
2025-03-10 17:57:11 +0100

    Add variables

---
+++ scr/main/java/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args){
+        int myInt = 10;
+        short myShort = 20;
+        long myLong = 30L;
+
+        float myFloat = 3.14f;
+        double myDouble = 4.56;
+
+        char myChar = 'A';
+
+        boolean myBoolean = true;
+
+        byte myByte = 127;
+        System.out.println(myInt);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myShort);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}
