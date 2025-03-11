5811e2717231577b5d6ebd034b419632500f9ef3
anna.severynova <anna.severynova@nixs.com>
2025-03-11 16:00:05 +0100

    Add variables

--- scr/main/java/Variables.java
+++ scr/main/java/Variables.java
@@ -1,17 +1,17 @@
 public class Variables {
     public static void main(String[] args){
-        int myInt = 10;
-        short myShort = 20;
-        long myLong = 30L;
+        int myInt = 10; // 32bit -2147483648 - 2147483647
+        short myShort = 20; // 16bit -32000 - 32000
+        long myLong = 30L; // 64bit
 
-        float myFloat = 3.14f;
-        double myDouble = 4.56;
+        float myFloat = 3.14f;// 32bit
+        double myDouble = 4.56;// 64bit
 
         char myChar = 'A';
 
         boolean myBoolean = true;
 
-        byte myByte = 127;
+        byte myByte = 127;// 8bit -128 - 127
         System.out.println(myInt);
         System.out.println(myShort);
         System.out.println(myLong);
