4b6ec66ff477bc0219b9cffb7f6983b51b2d76c5
anna.severynova <anna.severynova@nixs.com>
2025-03-12 15:49:47 +0100

    add classes

--- scr/main/java/HelloWorldInput.java
+++ scr/main/java/HelloWorldInput.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class HelloWorldInput {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input username");
-        String input = sc.nextLine();
-        System.out.println("Hello, "+input);
-
+printHelloName();
+    }
+    public static void printHelloName() {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter your name: ");
+       String name = sc.nextLine();
+       System.out.println("Hello, "+name);
     }
 }
--- scr/main/java/Main2.java
+++
@@ -1,6 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        String s = new String ("Hello World!");
-        System.out.println(s);
-    }
-}
