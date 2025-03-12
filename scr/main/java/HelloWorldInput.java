bab7d34a22dc8d3de836765de86293c58dcbfd86
anna.severynova <anna.severynova@nixs.com>
2025-03-12 17:17:23 +0100

    commit

--- scr/main/java/HelloWorldInput.java
+++ scr/main/java/HelloWorldInput.java
@@ -7,7 +7,8 @@ printHelloName();
     public static void printHelloName() {
        Scanner sc = new Scanner(System.in);
        System.out.println("Enter your name: ");
-       String name = sc.nextLine();
+       String name = sc.nextLine(); // в переменной name будем хранить данные, введенные с клавиатуры;
+                                    // nextLine - вызов Метода который принимает только строчные данные
        System.out.println("Hello, "+name);
     }
 }
