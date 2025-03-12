1a6d4339b6ae0858dc4b389581a34b5012a06557
anna.severynova <anna.severynova@nixs.com>
2025-03-12 11:37:05 +0100

    add Strings class

---
+++ scr/main/java/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args){
+        String myString = "Hello, World!"; // String - является классом,
+                                           // "Hello, World!" - объект класса Стринг,
+                                           //myString - ссылается на объект класса Стринг
+        String space = " ";
+        String name = "Anna";
+
+        //объеденение строк
+        System.out.println(myString+space+name);
+    }
+}
