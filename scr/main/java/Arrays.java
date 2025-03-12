2b3acffdbcd8b0b032a2938f86fd26debd1eaddd
anna.severynova <anna.severynova@nixs.com>
2025-03-12 18:20:28 +0100

    commit

---
+++ scr/main/java/Arrays.java
@@ -0,0 +1,7 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // создание массива
+        int[] numbers = new int[10]; // int[10] - размер массива
+        System.out.println(numbers[0]);
+    }
+}
