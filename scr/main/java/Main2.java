bff6ec962a8390e62ed7ab41e0073b679f0b1936
anna.severynova <anna.severynova@nixs.com>
2025-03-12 15:13:42 +0100

    add classes

---
+++ scr/main/java/Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        String s = new String ("Hello World!");
+        System.out.println(s);
+    }
+}
---
+++ scr/main/java/com/welcome/Hello.java
@@ -0,0 +1,7 @@
+package com.welcome;
+
+public class Hello {
+    public static void setupName(String name) {}
+    public static void welcome() {}
+    public static void byeBay() {}
+}
