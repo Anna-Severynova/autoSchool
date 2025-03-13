dc2009cc2bd25279a1a8fb4c28cd493e64e02ef0
anna.severynova <anna.severynova@nixs.com>
2025-03-13 12:58:20 +0100

    commit

--- scr/main/java/Methods.java
+++ scr/main/java/Methods.java
@@ -6,31 +6,33 @@ public class Methods {
     // создадим несколько конкретных Person
     public static void main(String[] args) {
         Person1 person = new Person1();
-        person.name = "Joe";
+        person.setName("Joe");//person.name = "Joe";
         person.age = 22;
 
         // вызов метода
         //person.sayHello();
-        //person.speak();
+        person.speak();
 
         Person1 person2 = new Person1();
-        person2.name = "Jane";
+        String s1 = "Jane";//person2.name = "Jane";
+        person2.setName(s1);
         person2.age = 21;
 
         // вызов метода
         //person2.sayHello();
-        //person2.speak();
+        person2.speak();
 
         // тип метода int
-        int year1 = person.calculateYearsToRetirement();
-        int year2 = person2.calculateYearsToRetirement();
-        System.out.println("First person is "+ year1);
-        System.out.println("Second person is "+ year2);
+        //int year1 = person.calculateYearsToRetirement();
+        //int year2 = person2.calculateYearsToRetirement();
+        //System.out.println("First person is "+ year1);
+        //System.out.println("Second person is "+ year2);
 
     }
 }
 
 class Person1 {
+
     String name;
     int age;
     int years;
@@ -50,5 +52,10 @@ class Person1 {
     int calculateYearsToRetirement(){
         int years = 65-age;
         return years;
+    }
+
+    //параметры метода
+    void setName(String username){
+        name = username;
     }
 }
