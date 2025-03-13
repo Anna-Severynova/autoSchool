c01ef36b965a5a0824d41fe6b6fc69391f2f3d28
anna.severynova <anna.severynova@nixs.com>
2025-03-13 11:49:05 +0100

    commit

--- scr/main/java/ClassesAndObjects.java
+++ scr/main/java/ClassesAndObjects.java
@@ -8,12 +8,12 @@ public class ClassesAndObjects {
         Person person = new Person();
         person.name = "Joe";
         person.age = 22;
-        System.out.println("My name is "+ person.name + "," + "and age is "+ person.age);
+        System.out.println("My name is " + person.name + "," + "and age is " + person.age);
 
         Person person2 = new Person();
         person2.name = "Jane";
         person2.age = 21;
-        System.out.println("My name is "+ person2.name + "," + "and age is "+ person2.age);
+        System.out.println("My name is " + person2.name + "," + "and age is " + person2.age);
 
     }
 }
---
+++ scr/main/java/Methods.java
@@ -0,0 +1,44 @@
+// У класса могут быть:
+// Данные (ПОЛЯ)
+// Действия, которые он может совершать (МЕТОДЫ)
+
+public class Methods {
+    // создадим несколько конкретных Person
+    public static void main(String[] args) {
+        Person1 person = new Person1();
+        person.name = "Joe";
+        person.age = 22;
+
+        // вызов метода
+        person.sayHello();
+        person.speak();
+
+
+        Person1 person2 = new Person1();
+        person2.name = "Jane";
+        person2.age = 21;
+
+        // вызов метода
+        person2.sayHello();
+        person2.speak();
+
+
+    }
+}
+
+class Person1 {
+    String name;
+    int age;
+
+    // создаем метод для того, чтобы человек представился
+    void speak(){
+        // вывести приветствие 3 раза
+        for (int i=0; i<4; i++){
+            System.out.println("My name is " + name + "," + "and age is " + age);
+        }
+    }
+
+    void sayHello(){
+        System.out.println("Hello");
+    }
+}
