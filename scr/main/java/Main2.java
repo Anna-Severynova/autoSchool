61e1d2dc37b4472b34b509c358305106d117e45c
anna.severynova <anna.severynova@nixs.com>
2025-03-13 14:37:30 +0100

    commit

--- scr/main/java/Main2.java
+++ scr/main/java/Main2.java
@@ -1,6 +1,19 @@
+import com.welcome.Hello;
+import java.util.Scanner;
+
 public class Main2 {
     public static void main(String[] args) {
-        String hello = new String("Hello");
-        System.out.println(hello);
+        Scanner sc = new Scanner(System.in);
+        //Запрос на ввод имени
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        //присвоить это имя классу Hello
+        Hello username = new Hello();
+        username.setName(String username);
+
+        username.welcome();
+        username.byBye();
+
     }
 }
--- scr/main/java/com/welcome/Hello.java
+++ scr/main/java/com/welcome/Hello.java
@@ -1,4 +1,19 @@
 package com.welcome;
 
 public class Hello {
+
+    private String name; // приватная переменная для хранения имени
+
+    String setName(String username) {
+        name = username;
+        return name;
+    }
+
+    void welcome(){
+        System.out.println("Welcome "+name);
+    }
+
+    void byBey(){
+        System.out.println("Bye "+name);
+    }
 }
