9cbf0d438d359b0ea363f6d08190e50adbdbbf0c
anna.severynova <anna.severynova@nixs.com>
2025-03-14 17:42:16 +0100

    SetGet class is created

--- scr/main/java/autoSchool/SetGet.java
+++ scr/main/java/autoSchool/SetGet.java
@@ -1,13 +1,20 @@
 package autoSchool;
 
+// желательно чтобы поля были видны только в пределах одного класса
+
 public class SetGet {
     public static void main(String[] args) {
         Students student = new Students();
         Students student2 = new Students();
-        student.setName("Anna");
-        student.setAge(23);
-        student.getName();
-        student.getAge();
+
+        // сетеры присваивают значение полям класса. геттеры - возвращают
+        student.setName("");
+        student.setAge(-1);
+
+        // выводим значения в мейн методе
+        System.out.println("Name is"+ " " + student.getName());
+        System.out.println("Age is" + " " + student.getAge());
+
 
     }
 
@@ -20,8 +27,13 @@ class Students {
     private String name;
     private int age;
 
+    // в сеттерах можно проверять корректность ввода
     public void setName(String username) {
-        name = username;
+        // проверяем что имя != null
+        if (username.isEmpty()){
+            System.out.println("Name is empty");
+        } else {
+            name = username;}
     }
 
     public String getName(){
@@ -29,7 +41,10 @@ class Students {
         }
 
     public void setAge(int userAge){
-        age = userAge;
+        if (userAge<0){
+            System.out.println("Age is not valid");
+        } else{
+        age = userAge;}
         }
 
     public int getAge(){
