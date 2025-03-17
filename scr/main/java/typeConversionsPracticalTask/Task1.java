f72043723e392471da293f4045cb0919118c055b
anna.severynova <anna.severynova@nixs.com>
2025-03-17 15:22:53 +0100

    update Task1

--- scr/main/java/typeConversionsPracticalTask/Task1.java
+++ scr/main/java/typeConversionsPracticalTask/Task1.java
@@ -3,8 +3,8 @@ package typeConversionsPracticalTask;
 public class Task1 {
     public static void main (String [] args) {
 Calculate calc = new Calculate();
-calc.setQ(21, 8);
-System.out.println("21 / 8 ="+" "+"целое число: "+calc.getQ()+" остаток: "+calc.getQ());
+calc.setResult(21, 8);
+System.out.println("21 / 8 ="+" "+"целое число: "+calc.getQ()+" остаток: "+calc.getW());
 
 
     }
@@ -14,11 +14,13 @@ private int q;
 private int w;
 private int a; // результат (целое число)
 private int b; // результат (число после точки)
+private double result;
 
-public void setQ(int q, int w) {
+public void setResult(int q, int w) {
     this.q = q;
     this.w = w;
-    this.a= q/w; // присваиваем целое число результата (без остатка)
+    this.result = (double) q/w;
+    this.a= (int)result; // присваиваем целое число результата (без остатка)
     this.b= q%w; // присваиваем остаток результата (без целого числа)
 }
 public int getQ() {
