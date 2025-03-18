4f3ef97074c72fe975ada669084d4121e604d5e8
anna.severynova <anna.severynova@nixs.com>
2025-03-18 09:45:02 +0100

    update Task2

--- scr/main/java/typeConversionsPracticalTask/Main.java
+++ scr/main/java/typeConversionsPracticalTask/Main.java
@@ -6,9 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Task1 calculate = new Task1();
         calculate.setResult(21, 8);
-        System.out.println("21 / 8 ="+" "+"целое число: "+calculate.getQ()+" остаток: "+calculate.getW());
-
-
+        System.out.println("21 / 8 =" + " " + "целое число: " + calculate.getQ() + " остаток: " + calculate.getW());
 
     }
+
 }
--- scr/main/java/typeConversionsPracticalTask/Task2.java
+++ scr/main/java/typeConversionsPracticalTask/Task2.java
@@ -3,6 +3,14 @@ package typeConversionsPracticalTask;
 import java.util.Scanner;
 
 public class Task2 {
+    int myNumber;
+    int number;
+    int resultNum1;
+    int resultNum2;
 
+
+static int getScan (){
+
+}
 
 }
