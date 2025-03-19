9a5292002b3da2f176f43c5d45cd16d2a78e17a4
anna.severynova <anna.severynova@nixs.com>
2025-03-19 17:42:10 +0100

    commit

--- scr/main/java/typeConversionsPracticalTask/Main.java
+++ scr/main/java/typeConversionsPracticalTask/Main.java
@@ -9,9 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Task1 calculate = new Task1();
-        // calculate.setResult(21, 8);
-        // System.out.println("21 / 8 =" + " " + "целое число: " + calculate.getQ() + " остаток: " + calculate.getW());
+         Task1 calculate = new Task1();
+         calculate.setResult(21, 8);
+         System.out.println("21 / 8 =" + " " + "целое число: " + calculate.getQ() + " остаток: " + calculate.getW());
 
         //Task2 calculate2 = new Task2();
         // calculate2.getScan();
