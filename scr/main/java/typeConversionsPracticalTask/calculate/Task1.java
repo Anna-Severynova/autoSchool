57fd61bf6b5c1dadec25f41e4f5a129e636baf15
anna.severynova <anna.severynova@nixs.com>
2025-03-21 15:34:22 +0100

    align code

--- scr/main/java/typeConversionsPracticalTask/calculate/Task1.java
+++ scr/main/java/typeConversionsPracticalTask/calculate/Task1.java
@@ -1,22 +1,21 @@
 package typeConversionsPracticalTask.calculate;
 
 public class Task1{
-    //private int q;private int w;
+
     private int a; // результат (целое число)
     private int b; // результат (число после точки)
     private double result;
 
     public void setResult(int q, int w) {
-       //this.q = q;
-       //this.w = w;
+
        this.result = (double) q/w;
        this.a= (int)result; // присваиваем целое число результата (без остатка)
        this.b= q%w; // присваиваем остаток результата (без целого числа)
-   }
+    }
     public int getQ() {
         return a;
     }
-   public int getW() {
+    public int getW() {
         return b;
     }
 
