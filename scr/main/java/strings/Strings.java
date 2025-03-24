4009a8eb10824f9a0ed2a9dbad27169ec44a60d9
anna.severynova <anna.severynova@nixs.com>
2025-03-24 16:14:26 +0100

    Starting work on task 1

---
+++ scr/main/java/strings/Strings.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        String str1 = "Hello World. Hello, Java!";
+        str1 = String.valueOf(str1.charAt(0));
+        System.out.println(str1);
+
+    }
+}
