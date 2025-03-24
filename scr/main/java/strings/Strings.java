b4cc1d29a5c672f6f32fbfff497898790eec8cea
anna.severynova <anna.severynova@nixs.com>
2025-03-24 17:57:18 +0100

    find the first and the last symbols (task 1)

--- scr/main/java/strings/Strings.java
+++ scr/main/java/strings/Strings.java
@@ -1,11 +1,21 @@
 package strings;
 
 public class Strings {
+    String middle = "Hello, World. Hello, Java!";
     public static void main(String[] args) {
-        String str1 = "Hello World. Hello, Java!";
-        str1 = String.valueOf(str1.charAt(0));
-        System.out.println(str1);
-        String str2 = "Hello World. Hello, Java!";
+        String str = "Hello, World. Hello, Java!";
+        String str1 = new String (new char[]{"Hello", })
+
+        str = str.toLowerCase();
+
+        int length = str.length();
+
+        String substring = str.substring(4, str.length()-12);
+
+        System.out.println(length);
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(str.length()-1));
+        System.out.println(substring);
 
     }
 }
