db8fa2dcea8c886dbe79e9628a8eaa325d8c14a5
Viktoriia Kyrylova <viktoriiak@playtika.com>
2025-03-25 16:23:45 +0100

    add example method to demonstrate string foreach loop

--- scr/main/java/autoSchool/stringExample/Main.java
+++ scr/main/java/autoSchool/stringExample/Main.java
@@ -2,8 +2,9 @@ package autoSchool.stringExample;
 
 public class Main {
     public static void main(String[] args) {
-        printWords(splitString("test testq testwo"));
-        System.out.println(maxLenght(splitString("testooooooo test testt")));
+//        printWords(splitString("test testq testwo"));
+//        System.out.println(maxLenght(splitString("testooooooo test testt")));
+        example();
     }
 
     public static String[] splitString(String str) {
@@ -29,5 +30,24 @@ public class Main {
             }
         }
         return max;
+    }
+
+    public static void example(){
+        String[] letters  = {"a", "b", "c", "d", "e", "f", "g", "h"};
+
+        for (String letter : letters) { //присваиваем переменной letter значение из массива letters
+            System.out.println("loop is started"); // выводим в консоль сообщение
+            System.out.println(letter + " option"); // выводим в консоль значение переменной letter и строку " option"
+
+            if (letter.equals("g")) { // если значение переменной letter равно "g"
+                System.out.println("g is found and break"); // выводим в консоль сообщение
+                break; // завершаем цикл
+            } else if (letter.equals("d")) { // если значение переменной letter равно "d"
+                System.out.println("D is found and continue"); // выводим в консоль сообщение
+            }
+
+            System.out.println("loop is ended"); // выводим в консоль сообщение
+        }
+
     }
 }
