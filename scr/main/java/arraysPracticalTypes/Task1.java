6759b69a40ebc4e22392052f3ceb67969c0fa25d
anna.severynova <anna.severynova@nixs.com>
2025-03-26 17:59:53 +0100

    start work with arrays

---
+++ scr/main/java/arraysPracticalTypes/Task1.java
@@ -0,0 +1,26 @@
+package arraysPracticalTypes;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 20 numbers: ");
+        //int numbers = sc.nextInt();
+        int num = 5;
+        int[] arr = new int[num];
+        int maxNum = 0;
+
+
+        for (int i = 0; i < arr.length; i++) { // переменную i используем как индекс массива
+            arr[i] = sc.nextInt();
+            if (arr[i] > maxNum) {
+                maxNum = arr[i];
+            }
+
+        }
+        System.out.println(maxNum);
+
+        }
+    }
+}
