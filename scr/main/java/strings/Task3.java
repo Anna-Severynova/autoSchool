a4dd1138f7d9bf2f9bbebb4c4df844215ed68816
anna.severynova <anna.severynova@nixs.com>
2025-03-26 16:07:32 +0100

    completed task 3

--- scr/main/java/strings/Task3.java
+++ scr/main/java/strings/Task3.java
@@ -8,22 +8,17 @@ public class Task3 {
         System.out.println("Enter the string: ");
         String myString = sc.nextLine();
 
-        String[] words = myString.split(" "); // создаем массив для вводимой строки
+        String[] words = myString.split(" "); // создаем массив 'words' для вводимой строки 'myString' и разделяем по пробелам
 
-        //создаем переменную для хранения самого длинного слова
-        String longestWord = "";
-
-        // создаем переменную для хранения длинны каждого слова в массиве
-        int longestWordLength = words[0].length();
+        String longestWord = ""; // переменная для хранения самого длинного слова из массива
+        int longestWordLength = 0; // переменная для хранения длины каждого слова
 
         for (int i = 0; i < words.length; i++) {
-            if (words[i].length() > longestWordLength) {
-                longestWord = words[i];
-                System.out.println(longestWord);
+            if (words[i].length() > longestWordLength) { //если длина первого слова больше чем ноль
+                longestWord = words[i]; // то в переменную longestWord присваиваем это слово (под индексом ноль)
+                longestWordLength = words[i].length(); // и обновляем значение длины слова в масиве (под индексом 0)
             }
         }
-
-
+        System.out.println(longestWord);
         }
-
 }
