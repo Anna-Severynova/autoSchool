3bc3d77e003fb5285ee72a908671b75413dea49e
anna.severynova <anna.severynova@nixs.com>
2025-03-28 18:02:35 +0100

    arrays: completed task3

--- scr/main/java/arraysPracticalTypes/Task2.java
+++ scr/main/java/arraysPracticalTypes/Task2.java
@@ -7,28 +7,42 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        String[] strings = initializeArray();
-        System.out.println("Your string array: " + Arrays.toString(strings));
-
-        System.out.println("Reverse array: "+Arrays.asList(reverseArray(strings)));
-    }
-
-    public static String[] initializeArray() {
+        String[] array = new String[10];
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 8 Strings:");
+        System.out.println("Enter 8 lines: ");
 
-        int num = 10;
-        String[] arr = new String[num]; // проинициализировали массив строк
         for (int i = 0; i < 8; i++) {
-            arr[i] = sc.nextLine();
+            array[i] = sc.nextLine();
         }
-        return arr;
+
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+
     }
 
-    public static String[] reverseArray(String[] arr) {
-
-        Collections.reverse(Arrays.asList(arr));
-        return arr;
+//    public static String[] initializeArray() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter 8 Strings:");
+//
+//        int num = 10;
+//        String[] arr = new String[num]; // проинициализировали массив строк
+//        for (int i = 0; i < 8; i++) {
+//            arr[i] = sc.nextLine();
+//        }
+//        //return arr;
+//        String[] arrReversed = new String[10];
+//        for (int i = 10; i > 0; i--) {
+//           arrReversed[i] = arr[i] ;
+//        }
+//        return arrReversed;
     }
-}
+//
+//
+//    public static String[] reverseArray(String[] arr) {
+//
+//        Collections.reverse(Arrays.asList(arr));
+//        return arr;
+//    }
+
 
