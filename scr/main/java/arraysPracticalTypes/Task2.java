05a70b1c72001f9e1a8f72df4af00dcd4219f7a6
anna.severynova <anna.severynova@nixs.com>
2025-03-31 12:05:37 +0200

    arrays: completed task4

--- scr/main/java/arraysPracticalTypes/Task2.java
+++ scr/main/java/arraysPracticalTypes/Task2.java
@@ -1,8 +1,4 @@
 package arraysPracticalTypes;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Task2 {
@@ -18,31 +14,5 @@ public class Task2 {
         for (int i = 9; i >= 0; i--) {
             System.out.println(array[i]);
         }
-
     }
-
-//    public static String[] initializeArray() {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter 8 Strings:");
-//
-//        int num = 10;
-//        String[] arr = new String[num]; // проинициализировали массив строк
-//        for (int i = 0; i < 8; i++) {
-//            arr[i] = sc.nextLine();
-//        }
-//        //return arr;
-//        String[] arrReversed = new String[10];
-//        for (int i = 10; i > 0; i--) {
-//           arrReversed[i] = arr[i] ;
-//        }
-//        return arrReversed;
-    }
-//
-//
-//    public static String[] reverseArray(String[] arr) {
-//
-//        Collections.reverse(Arrays.asList(arr));
-//        return arr;
-//    }
-
-
+}
--- scr/main/java/arraysPracticalTypes/Task3.java
+++ scr/main/java/arraysPracticalTypes/Task3.java
@@ -17,7 +17,6 @@ public class Task3 {
             arrNum[i] = array[i].length();
             System.out.println(arrNum[i]);
         }
-
     }
 }
 
