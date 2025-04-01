2a765dbd96eaeacb7deb40ff2efb28fa2b84652c
anna.severynova <anna.severynova@nixs.com>
2025-04-01 12:49:11 +0200

    conditional operators: completed task12

---
+++ scr/main/java/conditionalOperatorsPracticeTasks/Task13.java
@@ -0,0 +1,9 @@
+package conditionalOperatorsPracticeTasks;
+
+public class Task13 {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
