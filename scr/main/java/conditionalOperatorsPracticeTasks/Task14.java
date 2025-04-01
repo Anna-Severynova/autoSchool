7d9cfa4da298c29cfed8187db5235eb7cc2301c3
anna.severynova <anna.severynova@nixs.com>
2025-04-01 14:26:17 +0200

    conditional operators: completed task12(with 'for' loop), completed task13 and task 14, task 15 - in progress.

--- scr/main/java/conditionalOperatorsPracticeTasks/Task14.java
+++ scr/main/java/conditionalOperatorsPracticeTasks/Task14.java
@@ -9,7 +9,6 @@ public class Task14 {
         int m = sc.nextInt();
         System.out.println("Enter n: ");
         int n = sc.nextInt();
-
         for (int i = 0; i < m; i++) { // количество строк
             for (int j = 1; j < n; j++) { // символы в строке
                 System.out.print("8");
