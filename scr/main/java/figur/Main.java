451d6f40827954ac0260c6324e64beda9ca57746
anna.severynova <anna.severynova@nixs.com>
2025-04-07 14:20:01 +0200

    OOP: implement array in the Main class for output areas of the 10 figures

--- scr/main/java/figur/Main.java
+++ scr/main/java/figur/Main.java
@@ -1,24 +1,38 @@
 package figur;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle("Circle");
-        System.out.println(circle.getName());
-        circle.coordinates();
-        System.out.println("Coordinates for the Circle: " + circle.getCoordinates());
-        System.out.println("Area for the Circle: " + circle.area());
 
+        Figures[] arrShape = new Figures[10];
+        Random shapes = new Random(3);
 
-        Triangle triangle = new Triangle("Triangle");
-        System.out.println(triangle.getName());
-        triangle.coordinates();
-        System.out.println("Coordinates for triangle: " + triangle.getCoordinates());
-        System.out.println("Area for the triangle: " + triangle.area());
+        for (int i = 0; i < arrShape.length; i++) { // массив для заполнения фигурами+методы
 
-        Square square = new Square("Square");
-        System.out.println(square.getName());
-        square.coordinates();
-        System.out.println("Coordinates for the Square: " + square.getCoordinates());
-        System.out.println("Area for the Square: " + square.area());
+            int type = shapes.nextInt(3); // 0 - круг, 1 - квадрат, 2 - треугольник
+
+            if (type == 0) {
+                arrShape[i] = new Circle("Circle");
+
+            } else if (type == 1) {
+                arrShape[i] = new Square("Square");
+
+            } else {
+                arrShape[i] = new Triangle("Triangle");
+
+            }
+            arrShape[i].coordinates();
+            arrShape[i].area();
+        }
+        Arrays.sort(arrShape, (a, b) -> Double.compare(a.area(), b.area()));
+        for (int i = 0; i < arrShape.length; i++) { // вывод уже отсортированного массива
+            Figures f = arrShape[i];
+            String parameters = f.getName();
+            double area = f.area();
+            System.out.println("name of the shape: " + parameters + ", " + "area: " + area);
+            System.out.println();
+        }
     }
 }
