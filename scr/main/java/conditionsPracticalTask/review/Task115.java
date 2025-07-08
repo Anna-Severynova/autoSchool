4f7e4dfb8d497f198223df59b3420d1989f9cfa3
Anna-Severynova <anna.severynova@nixs.com>
2025-07-08 15:40:28 +0200

    Completed tasks from 1 to 14 (conditional operators and loops)

--- scr/main/java/conditionsPracticalTask/review/Task115.java
+++ scr/main/java/conditionsPracticalTask/review/Task115.java
@@ -2,9 +2,9 @@ package conditionsPracticalTask.review;
 
 public class Task115 {
     public static void main(String[]args){
-        for(int i = 0; i<10; i++){
-            for(int j = 0; j<10;j++){
-                System.out.println("8");
+        for(int i = 1; i<=11; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("8");
             }
             System.out.println();
         }
