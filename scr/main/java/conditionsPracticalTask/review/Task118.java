abd8f279d5bc6eb6b3bc90098364a0657e827743
Anna-Severynova <anna.severynova@nixs.com>
2025-07-08 16:13:52 +0200

    Completed task18

---
+++ scr/main/java/conditionsPracticalTask/review/Task118.java
@@ -0,0 +1,11 @@
+package conditionsPracticalTask.review;
+
+public class Task118 {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+            Thread.sleep(500);
+        }
+        System.out.print("Boom!");
+    }
+}
