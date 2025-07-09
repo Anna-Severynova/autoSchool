07c810450c49d3e74ec019972f16dade4abc0808
Anna-Severynova <anna.severynova@nixs.com>
2025-07-09 15:01:15 +0200

    Restructured packages for previous task review

---
+++ scr/main/java/review/conditionsPractical/Task111.java
@@ -0,0 +1,17 @@
+package review.conditionsPractical;
+
+public class Task111 {
+    public static void main (String [] args){
+        int i = 0; // считаем количество строки
+        while (i<10){
+            int j = 0; // счетчик для подсчета символов в каждой строчке
+
+            while (j<10){
+                System.out.print("S");
+                j++;
+            }
+            System.out.println();// после каждой итерации переходим на новую строку
+            i++;
+        }
+    }
+}
