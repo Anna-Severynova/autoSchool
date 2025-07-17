e3a3775f6133ef4b64ec0382546bf4388d253910
Anna-Severynova <anna.severynova@nixs.com>
2025-07-17 17:07:52 +0200

    Completed task 17 on Collections.

---
+++ scr/main/java/Collections/Task17.java
@@ -0,0 +1,28 @@
+package Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task17 {
+    public static void main(String[] args) {
+        
+        Map<String, String> mapOfNames = new HashMap<>();
+
+        mapOfNames.put("Osipenko", "Maksim");
+        mapOfNames.put("Potapova", "Olena");
+        mapOfNames.put("Shubina", "Ekaterina");
+        mapOfNames.put("Tavridow", "Pavel");
+        mapOfNames.put("Klimenko", "Olexander");
+        mapOfNames.put("Fedorov", "Fedor");
+        mapOfNames.put("Fedorov", "Petro");
+        mapOfNames.put("Petrov", "Petro");
+        mapOfNames.put("Vasilenko", "Olena");
+        mapOfNames.put("Vasilenko", "Pavel");
+
+        for (Map.Entry<String, String> i : mapOfNames.entrySet()) {
+            String key = i.getKey();
+            String value = i.getValue();
+            System.out.println(key + " - " + value);
+        }
+    }
+}
