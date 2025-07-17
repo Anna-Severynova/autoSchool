79baed3c8f62816685fe2fe2ca1c68d844824c78
Anna-Severynova <anna.severynova@nixs.com>
2025-07-17 16:11:15 +0200

    Completed task 14 on Collections. ( I also left some commented-out code as examples )

--- scr/main/java/Collections/Task14.java
+++ scr/main/java/Collections/Task14.java
@@ -16,5 +16,23 @@ public class Task14 {
         map.put("Lozova", "Anna");
         map.put("Lisenko", "Oksana");
 
+        String name = "Anna";
+        int count = 0;
+        for (Map.Entry<String, String> i : map.entrySet()) {
+            if (i.getValue() == name) {
+                count++;
+            }
+        }
+        System.out.println(count);
+//        int count = 0;
+//        String[] values = map.values().toArray(new String[0]);
+//        for (int i = 0; i < values.length; i++) {
+//            for (int j = i + 1; j < values.length; j++) {
+//                if (values[i].equals(values[j])) {
+//                    count++;
+//                }
+//            }
+//        }
+//        System.out.println("The number people with same name: " + count);
     }
 }
