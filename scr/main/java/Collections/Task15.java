344f30a4978814fdce7cd498aaec0d9024eb8075
Anna-Severynova <anna.severynova@nixs.com>
2025-09-19 14:44:43 +0200

    complited task 15: add check for the same Last name

---
+++ scr/main/java/Collections/Task15.java
@@ -0,0 +1,40 @@
+package Collections;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Task15 {
+    public static void main (String[] args){
+        Map<String, LocalDate> map = new HashMap<>();
+            map.put("Anna", LocalDate.parse("1996-07-25")); // del
+            map.put("Olena", LocalDate.parse("1996-01-25"));
+            map.put("Stanislav", LocalDate.parse("1996-06-25")); // del
+            map.put("Peter", LocalDate.parse("1996-08-25")); // del
+            map.put("David", LocalDate.parse("1996-03-25"));
+
+            System.out.println("Before delete:");
+            for(Map.Entry<String, LocalDate> i: map.entrySet()){
+                String name = i.getKey();
+                LocalDate date = i.getValue();
+                System.out.println(name+": "+date);
+            }
+
+        Iterator<Map.Entry<String, LocalDate>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            LocalDate date = iterator.next().getValue();
+            if (date.getMonthValue() == 6 || date.getMonthValue() == 7 || date.getMonthValue() == 8) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("After delete:");
+        for (Map.Entry<String, LocalDate> i: map.entrySet()){
+            String name = i.getKey();
+            LocalDate date = i.getValue();
+            System.out.println(name+": "+date);
+        }
+    }
+}
