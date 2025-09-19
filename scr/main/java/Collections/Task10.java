74705cfb79027ad23584fbcb31f9f3325c1d6d0e
Anna-Severynova <anna.severynova@nixs.com>
2025-09-19 12:10:32 +0200

    edited task 10 - so now the system request input string from keyboard, and input it each from a new line

--- scr/main/java/Collections/Task10.java
+++ scr/main/java/Collections/Task10.java
@@ -5,11 +5,17 @@ import java.util.*;
 public class Task10 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 10 strings: ");
+
         Deque<String> tenStrings = new LinkedList<>();
 
         for (int i = 0; i < 10; i++){
-            tenStrings.addFirst("str"+i);
+            tenStrings.addFirst(sc.nextLine());
         }
-        System.out.println(tenStrings);
+
+        for(String str: tenStrings){
+            System.out.println(str);
+        }
     }
 }
