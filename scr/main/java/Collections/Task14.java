ccfed8dfc57b1cbbaa6e4b0cfbf67656da3a1bb7
Anna-Severynova <anna.severynova@nixs.com>
2025-09-19 14:14:11 +0200

    edited task 14: add check for the same Last name

--- scr/main/java/Collections/Task14.java
+++ scr/main/java/Collections/Task14.java
@@ -14,25 +14,15 @@ public class Task14 {
         map.put("Yakovenko", "Pavel");
         map.put("Osipov", "Pavel");
         map.put("Lozova", "Anna");
-        map.put("Lisenko", "Oksana");
+        map.put("Lozova", "Oksana");
 
         String name = "Anna";
         int count = 0;
         for (Map.Entry<String, String> i : map.entrySet()) {
-            if (i.getValue() == name) {
+            if (i.getValue() == name || i.getKey() == name) {
                 count++;
             }
         }
-        System.out.println(count);
-//        int count = 0;
-//        String[] values = map.values().toArray(new String[0]);
-//        for (int i = 0; i < values.length; i++) {
-//            for (int j = i + 1; j < values.length; j++) {
-//                if (values[i].equals(values[j])) {
-//                    count++;
-//                }
-//            }
-//        }
-//        System.out.println("The number people with same name: " + count);
+        System.out.println("The number people with the same first or last name: " + count);
     }
 }
