43335615439ff7f6a16516443706e7c73a1d4f87
Anna-Severynova <anna.severynova@nixs.com>
2025-09-26 17:59:38 +0200

    add the streams to the task for Stream API

--- scr/main/java/streamApiPracticalTasks/Task1.java
+++ scr/main/java/streamApiPracticalTasks/Task1.java
@@ -15,56 +15,62 @@ import java.util.List;
 public class Task1 {
     public  static void main(String[] args){
         List<String> list = new ArrayList<>();
-        list.add("Test");
+        list.add("aTest");
         list.add("");
         list.add("dTest11");
-        list.add("Test111");
+        list.add("bTest111");
+        list.add("");
+        list.add("mdTest");
+        list.add("wTest1");
+        list.add("qTest11");
+        list.add("");
+        list.add("wTestd1111");
+        list.add("eTest");
+        list.add("");
+        list.add("rTest11");
+        list.add("tTest111");
+        list.add("");
+        list.add("yTest");
+        list.add("uTesdt1");
+        list.add("iTest11");
+        list.add("oTest111");
+        list.add("pTest1111");
+        list.add("dTest");
+        list.add("rTest1");
+        list.add("aTest11");
+        list.add("sTest111");
         list.add("");
         list.add("dTest");
-        list.add("Test1");
-        list.add("Test11");
+        list.add("fTest1");
+        list.add("gTest11");
+        list.add("hTest111");
+        list.add("jTest1111");
+        list.add("kTest");
+        list.add("lTest1");
+        list.add("zTest11");
+        list.add("xTest111");
+        list.add("cTest1111");
+        list.add("vTest");
+        list.add("bTest1");
+        list.add("nTest11");
         list.add("");
-        list.add("Testd1111");
-        list.add("Test");
+        list.add("mTest1111");
+        list.add("mTest");
+        list.add("rTest1");
+        list.add("tTest11");
+        list.add("mTest111");
         list.add("");
-        list.add("Test11");
-        list.add("Test111");
-        list.add("");
-        list.add("Test");
-        list.add("Tesdt1");
-        list.add("Test11");
-        list.add("Test111");
-        list.add("Test1111");
-        list.add("dTest");
-        list.add("Test1");
-        list.add("Test11");
-        list.add("Test111");
-        list.add("");
-        list.add("Test");
-        list.add("Test1");
-        list.add("Test11");
-        list.add("Test111");
-        list.add("Test1111");
-        list.add("Test");
-        list.add("Test1");
-        list.add("Test11");
-        list.add("Test111");
-        list.add("Test1111");
-        list.add("Test");
-        list.add("Test1");
-        list.add("Test11");
-        list.add("");
-        list.add("Test1111");
-        list.add("Test");
-        list.add("Test1");
-        list.add("Test11");
-        list.add("Test111");
-        list.add("");
-        list.add("Test");
-        list.add("Test1");
-        list.add("Test11");
+        list.add("nTest");
+        list.add("rTest1");
+        list.add("qTest11");
         list.add("dTest111");
-        list.add("Test1111");
+        list.add("aTest1111");
+
+        list.stream()
+                .filter(a -> !a.contains("d") && !a.contains("D") && !a.isEmpty())
+                .map(a -> a + ": " + a.length() + " -- is lenght of the string")
+                .sorted()
+                .forEach(System.out::println);
 
 
     }
