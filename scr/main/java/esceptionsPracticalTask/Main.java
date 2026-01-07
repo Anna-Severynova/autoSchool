5ef578a88fcca3e893ce60c4768fc1342967804a
Anna-Severynova <anna.severynova@nixs.com>
2026-01-07 14:24:51 +0100

    completed the Exceptions Practical Task (2)

--- scr/main/java/esceptionsPracticalTask/Main.java
+++ scr/main/java/esceptionsPracticalTask/Main.java
@@ -1,29 +1,32 @@
 // Task:
-// Create two classes.
-//Implement the next code inside them:
-//Thread.sleep(); with parameter of 3 seconds
-//in two different ways
-// via try/catch
-//  specify the type of generated exception when declaring the method
-//      2. Create class Main.
-//In any place of the code, insert the following line
-// int zero = 5/0;
-//When executing this code in the console, we will get an exception java.lang.ArithmeticException
+//
+// 1. Create two classes.
+//
+// Implement the next code inside them:
+// Thread.sleep(); with parameter of 3 seconds
+//  in two different ways
+//   - via try/catch
+//   - specify the type of generated exception when declaring the method
+//
+// 2. Create class Main.
+//
+// In any place of the code, insert the following line: 'int zero = 5/0;'
+// When executing this code in the console, we will get an exception java.lang.ArithmeticException
 // Use try/catch to catch the generated exception and replace it with the text You shall not fall!
 
 package esceptionsPracticalTask;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        ExceptionsTryCatch excTryCatch = new ExceptionsTryCatch();
-        ExceptionsThrows excThrows = new ExceptionsThrows();
-        excTryCatch.sleepTryCatch();
-        excThrows.sleepThrows();
+        ExceptionsThrows sleepExcThrows = new ExceptionsThrows();
+        ExceptionsTryCatch sleepExcTryCatch = new ExceptionsTryCatch();
+        sleepExcThrows.sleepThrows();
+        sleepExcTryCatch.sleepTryCatch();
+//      int zero = 5/0;
+//      System.out.println(zero);
 
         try {
-            System.out.println("Division by zero test:");
-            int result = 5 / 0;
-            System.out.println("Division by zero test:");
+            int zero1 = 5 / 0;
         } catch (ArithmeticException e) {
             System.out.println("You shall not fall!");
         }
