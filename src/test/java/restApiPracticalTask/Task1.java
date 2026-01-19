c923dc1e64e9147071bf9df6a944ad1ae772e948
Anna-Severynova <anna.severynova@nixs.com>
2026-01-19 18:03:31 +0100

    Completed the Task1

--- src/test/java/restApiPracticalTask/Task1.java
+++ src/test/java/restApiPracticalTask/Task1.java
@@ -1,6 +1,5 @@
 package restApiPracticalTask;
 
-
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -18,6 +17,26 @@ public class Task1 {
         JsonPath jsonPath = response.jsonPath();
         Map<String, Object> keysURL = jsonPath.getMap("paths"); // парсим Json чтобы собрать юрл (ключи)
         Set<String> endpoints = keysURL.keySet();
-        System.out.println(endpoints);
+
+        // создаем цикл, который будет:
+        //   1 - проходить по каждому элементу Set.
+        //   2 - вызывать каждый ендпоинт
+        //   3 - проверять статус код
+        //   4 - если НЕ 200 - сохранять юрл и статус коды в Мапу
+        Map<String, Integer> urlAndStatusCode = new HashMap<>();
+        for (String endpoint : endpoints) {
+            String safeEndpoint = endpoint
+                    .replace("{", "%7B")
+                    .replace("}", "%7D"); //конвертируем ендроинты с параметрами в "читабельный" для Rest Assured вид
+            Response response1 = RestAssured.given()
+                    .urlEncodingEnabled(false)
+                    .header("User-Agent", "Learning Automation")
+                    .get("https://nghttp2.org/httpbin" + safeEndpoint);
+           int statusCode = response1.getStatusCode();
+            if (statusCode!=200){
+               urlAndStatusCode.put("https://nghttp2.org/httpbin"+safeEndpoint, response1.getStatusCode());
+            }
+        }
+        System.out.println(urlAndStatusCode);
     }
 }
