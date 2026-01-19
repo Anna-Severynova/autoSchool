7648b4d88723e4ddbfe285f020f7b72f329733d0
Anna-Severynova <anna.severynova@nixs.com>
2026-01-19 16:55:21 +0100

    Parsed spec.json to get all available endpoints

--- src/test/java/restApiPracticalTask/Task1.java
+++ src/test/java/restApiPracticalTask/Task1.java
@@ -2,32 +2,21 @@ package restApiPracticalTask;
 
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Task1 {
     public static void main(String[] args) {
-        Response request = RestAssured.given()
+        Response response = RestAssured.given()
                 .header("User-Agent", "Learning Automation")
-                .get("https://nghttp2.org/httpbin/anything");
-
-        Map<String, Integer> mapURLandStatusCodes = new HashMap<>();
-        int status = request.getStatusCode();
-        if (status != 200) {
-            mapURLandStatusCodes.put("https://nghttp2.org/httpbin/anything", request.getStatusCode());
-        }
-
-        Response request2 = RestAssured.given()
-                .header("User-Agent", "Learning Automation")
-                .get("https://nghttp2.org/httpbin/basic-auth/%7Buser%7D/%7Bpasswd%7D");
-
-        int status2 = request2.getStatusCode();
-        if (status2 != 200) {
-            mapURLandStatusCodes.put("https://nghttp2.org/httpbin/basic-auth/%7Buser%7D/%7Bpasswd%7D", request2.getStatusCode());
-
-        }
-        System.out.println(mapURLandStatusCodes);
+                .accept("application/json")
+                .get("https://nghttp2.org/httpbin/spec.json");
+        JsonPath jsonPath = response.jsonPath();
+        Map<String, Object> keysURL = jsonPath.getMap("paths"); // парсим Json чтобы собрать юрл (ключи)
+        System.out.println(keysURL.keySet());
     }
 }
