92e24251729148da3f770fe73ebb2d7a7e4568fe
Anna-Severynova <anna.severynova@nixs.com>
2026-01-26 17:39:16 +0100

    Modifyed the Task33_Test -> add the @BeforeEach

--- src/test/java/jUnit5/Task33_Test.java
+++ src/test/java/jUnit5/Task33_Test.java
@@ -1,8 +1,7 @@
 package jUnit5;
 
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
+import basicCommandForTesting.UniqueLangs;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,78 +11,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class Task33_Test {
+
+    private Set<String> testResult;
+
+    @BeforeEach
+    void setUpData() {
+        UniqueLangs uniqueLangs = new UniqueLangs();
+        testResult = uniqueLangs.setUniqueLangs();
+    }
+
     @Test
-    @DisplayName("setUniqueLangs method returns only unique languages")
-    public void shouldReturnOnlyUniqueLanguages() {
-        System.out.println();
-        System.out.println("Task 3.1: Return the unique languages.");
-        System.out.println();
-        System.out.println(setUniqueLangs());
-        Set<String> languages = setUniqueLangs();
-        Map<String, Long> populations = countPopulationByLang(languages);//в Map сохраняю метод(с арг Сета из уник.яз. Этот метод: проходит по всему Сету уник.языков, и кладет в результирующую Мапу язык + сумму популяции (которую получаем через приватный метод))
-        System.out.println();
-        System.out.println("Task 3.2: Count the sum of populations by languages.");
-        System.out.println();
-        System.out.println(populations);
+    @DisplayName("setUniqueLangs method returns NOT null")
+    void shouldReturnNotNull() {
 
-        //вызываем метод, который тестируем
-        Set<String> result = setUniqueLangs();
-
-        //1й тест - провеверяем, что метод возвращает NOT null
         assertNotNull(
-                result,
+                testResult,
                 "Languages should NOT be null"
         );
+    }
 
-        //2й тест - проверяем языки на уникальность
-        List<String> languagesList = new ArrayList<>(languages);
+    @Test
+    @DisplayName("setUniqueLangs method returns only unique languages")
+    void shouldReturnOnlyUniqueLanguages() {
+
+        List<String> languagesList = new ArrayList<>(testResult);
+        Set<String> checkSetLangs = new HashSet<>(languagesList);
 
         assertEquals(
                 languagesList.size(),
-                new HashSet<>(languagesList).size(),
+                checkSetLangs.size(),
                 "Languages set should contain only unique values"
         );
-    }
-    public static Set<String> setUniqueLangs() {
-        Response response = RestAssured.given()
-                .header("User-Agent", "Learning Automation")
-                .get("https://restcountries.com/v3.1/all?fields=name,languages");
-        JsonPath jsonPath = response.jsonPath();
-
-        // получаем весь массив стран - List из country-объектов
-        List<Map<String, Object>> listAllCountries = jsonPath.getList("");
-
-        //создаем Set для хранения всех уникальных языков
-        Set<String> setUniqueLang = new HashSet<>();
-
-        //чтобы достать languages, проходимся циклом по всем странам
-        for (Map<String, Object> country : listAllCountries) {
-            Map<String, String> mapLang = (Map<String, String>) country.get("languages");
-            setUniqueLang.addAll(mapLang.keySet());
-        }
-        return setUniqueLang;
-    }
-
-    public static Map<String, Long> countPopulationByLang(Set<String> langs) {
-        Map<String, Long> mapFinalResult = new HashMap<>();
-        for (String lang : langs) {
-            mapFinalResult.put(lang, findPopulationByLang(lang));
-        }
-        return mapFinalResult;
-    }
-
-    private static long findPopulationByLang(String lang) {//нахожу и суммирую популяцию по языку (For each language code get json from https://restcountries.com/v3/lang/[language code]. In the received json, summarize the population values)
-
-        Response response = RestAssured.given()
-                .header("User-Agent", "Learning Automation")
-                .get("https://restcountries.com/v3.1/lang/" + lang);
-        JsonPath jsonPath = response.jsonPath();
-        List<Number> listAllPopulationByLang = jsonPath.getList("population");
-
-        long sum = 0;
-        for (Number population : listAllPopulationByLang) {
-            sum = sum + population.longValue();
-        }
-        return sum;
     }
 }
