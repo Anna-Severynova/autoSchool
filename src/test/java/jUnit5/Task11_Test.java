5156cd1c713dc3797bbc0f016895554bdf8dd386
Anna-Severynova <anna.severynova@nixs.com>
2026-01-26 11:06:27 +0100

    Creat a test for Task 1

---
+++ src/test/java/jUnit5/Task11_Test.java
@@ -0,0 +1,58 @@
+package jUnit5;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class Task11_Test {
+    @Test
+    @DisplayName("All endpoints from spec.json should return NON-200 OK")
+    void allEndpointsShouldReturnNON200() {
+        Response response = RestAssured.given()
+                .header("User-Agent", "Learning Automation")
+                .accept("application/json")
+                .get("https://nghttp2.org/httpbin/spec.json");
+        JsonPath jsonPath = response.jsonPath();
+        Map<String, Object> keysURL = jsonPath.getMap("paths"); // парсим Json чтобы собрать юрл (ключи)
+        Set<String> endpoints = keysURL.keySet();
+
+        // создаем цикл, который будет:
+        //   1 - проходить по каждому элементу Set.
+        //   2 - вызывать каждый ендпоинт
+        //   3 - проверять статус код
+        //   4 - если НЕ 200 - сохранять юрл и статус коды в Мапу
+        Map<String, Integer> urlAndStatusCode = new HashMap<>();
+        for (String endpoint : endpoints) {
+            String safeEndpoint = endpoint
+                    .replace("{", "%7B")
+                    .replace("}", "%7D"); //конвертируем ендроинты с параметрами в "читабельный" для Rest Assured вид
+            Response response1 = RestAssured.given()
+                    .urlEncodingEnabled(false)
+                    .header("User-Agent", "Learning Automation")
+                    .get("https://nghttp2.org/httpbin" + safeEndpoint);
+            int statusCode = response1.getStatusCode();
+            if (statusCode != 200) {
+                urlAndStatusCode.put("https://nghttp2.org/httpbin" + safeEndpoint, response1.getStatusCode());
+            }
+        }
+        System.out.println(urlAndStatusCode);
+
+        //тестируем, что сохраненные в Map urlAndStatusCode коды !=200
+        for (Integer statusCode : urlAndStatusCode.values()) {
+            assertNotEquals(
+                    200,
+                    statusCode,
+                    "Map contains endpoint with 200 status code"
+            );
+        }
+    }
+}
